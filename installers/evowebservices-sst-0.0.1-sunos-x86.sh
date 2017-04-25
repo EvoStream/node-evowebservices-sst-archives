97d8ea7f77efac7a0a613e81a647576ec8bfaaca
toni08 <tpanganiban@evostream.com>
2017-04-25 13:57:07 +0800

    Second commit. Adding codes

---
+++ installers/evowebservices-sst-0.0.1-sunos-x86.sh
@@ -0,0 +1,45 @@
+﻿#!/bin/sh
+#
+# Script to install node JS, ERS dependencies, and the actual ERS code.
+#
+
+# Get the needed parameters
+OS=sunos
+ARCH=x86
+VER=4.1.1
+NODE_JS=http://nodejs.org/dist/v$VER/node-v$VER-$OS-$ARCH.tar.gz
+
+# Download and extract needed node JS
+which node
+if [ $? != 0 ]; then 
+    echo "Node.js DOES NOT exists.... Installing Node.js first...." 
+
+    wget $NODE_JS 
+
+    # Next, execute the following command to install the Node.js binary package in /usr/local/:
+    tar zxf node-v$VER-$OS-$ARCH.tar.gz -C /usr/local --strip-components 1 
+    if [ $? != 0 ]; then echo 'Node.js: INSTALLATION FAILED! Please see errors below:'; exit 1; fi
+    
+    echo "Node.js: SUCCESSFUL INSTALLATION!" 
+
+    
+else
+    echo "Node.js already exists" 
+fi 
+
+# Install evowebservices-sst
+echo 'Installing NPM evowebservices-sst...'
+npm install https://github.com/EvoStream/node-evowebservices-sst/tarball/master
+if [ $? != 0 ]; then echo 'evowebservices-sst: INSTALLATION FAILED! Please see errors below:'; exit 1; fi
+
+echo "evowebservices-sst: SUCCESSFUL INSTALLATION!" 
+
+# Change to where evowebservices-sst is
+cd node_modules/evowebservices-sst
+
+# Start evowebservices-sst
+echo 'Starting evowebservices-sst...'
+npm start 
+if [ $? != 0 ]; then echo 'Could not start evowebservices-sst!'; exit 1; fi
+
+exit 0;
---
+++ installers/evowebservices-sst-0.0.1-win-x64.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+
+echo creating installation folder in node_evowebservices-sst\.... 
+IF NOT EXIST node_evowebservices MD node_evowebservices-sst
+
+echo changing directory to node_evowebservices-sst\......
+CD node_evowebservices-sst
+
+echo getting evowebservices-sst on github...
+call npm install --save https://github.com/EvoStream/node-evowebservices-sst/tarball/master
+
+echo changing directory to evowebservices....
+cd node_modules\evowebservices-sst
+
+echo starting evowebservices-sst using npm....
+call npm start
+
+echo.
+echo evowebservices-sst INSTALLED ON %cd%\node_evowebservices\node_modules\evowebservices-sst
+echo PRESS ANY KEY TO EXIT. 
+echo.
+pause >nul
